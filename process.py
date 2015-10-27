67f95a93ec8db81cf23328c6682f619c7248e065
Wojciech Doganowski <W.Doganowski@adbglobal.com>
2015-10-27 22:09:15 +0100

    Updated

--- process.py
+++ process.py
@@ -1,4 +1,3 @@
-# coding: cp1250
 #!/usr/bin/python
 # coding: utf-8
 
@@ -71,7 +70,6 @@ def processLine(line, data):
 processFile(args.input_file)
 
 if args.format == 'csv':
-  with open(args.output_file, encoding='utf-8', mode='wb') as csvfile:
   with open(args.output_file, encoding='cp1250', mode='wt') as csvfile:
     csvwriter = csv.DictWriter(csvfile, fieldnames=['imie','nazwisko','email','telefon','uczelnia','wydzial','doswiadczenie','motywacja'], 
       dialect=csv.excel, delimiter=';')
@@ -81,5 +79,5 @@ if args.format == 'csv':
 
 elif args.format == 'txt':
   for data in database:
-    print '{0} {1} {2} {3} {4} {5} {6} {7}"'.format(data.imie, data.nazwisko, data.email, data.telefon, data.uczelnia, data.wydzial, data.doswiadczenie, data.motywacja)     print '{0} {1} {2} {3} {4} {5} {6} {7}"'.format(data.imie, data.nazwisko, data.email, data.telefon, data.uczelnia, data.wydzial, data.doswiadczenie, data.motywacja)
+    print '{0} {1} {2} {3} {4} {5} {6} {7}"'.format(data.imie, data.nazwisko, data.email, data.telefon, data.uczelnia, data.wydzial, data.doswiadczenie, data.motywacja)
     pass
